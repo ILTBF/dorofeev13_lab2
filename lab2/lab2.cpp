0f6be5dc3db74d8bddf90e1733634d17387fea99
ILTBF <nikitka153@mail.ru>
2022-11-28 04:32:08 +0700

    Создана структура Triangle

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -30,6 +30,32 @@ struct Point {
 
 };
 
+struct Triangle {
+    Point abc[3];
+    //инициализация - конструктор
+    Triangle(Point A = Point(0, 0), Point B = Point(0, 0), Point C = Point(0, 0)) {
+        abc[0] = A;
+        abc[1] = B;
+        abc[2] = C;
+    }
+    //вывод информации о координатах треугольника
+    void print() {
+        cout << "\n\n Координаты треугольника: ";
+        for (int i = 0; i < 3; i++) {
+            abc[i].print();
+        }
+    }
+    //Метод для вычисление длин сторон треугольника
+    double Length() {
+        double a, b, c;
+        a = sqrt((abc[0].x - abc[1].x) * (abc[0].x - abc[1].x) + (abc[0].y - abc[1].y) * (abc[0].y - abc[1].y));
+        b = sqrt((abc[0].x - abc[2].x) * (abc[0].x - abc[2].x) + (abc[0].y - abc[2].y) * (abc[0].y - abc[2].y));
+        c = sqrt((abc[1].x - abc[2].x) * (abc[1].x - abc[2].x) + (abc[1].y - abc[2].y) * (abc[1].y - abc[2].y));
+        cout << "\n Длина стороны A: " << a << "\n Длина стороны B: " << b << "\n Длина стороны C: " << c;
+        return 0;
+    }
+};
+
 int main()
 {
     setlocale(LC_ALL, "Russian");
